c89c4a3be8b1560705d8af1f42135cfd8aa28216
Renan Fernandes Marques <165963782+Renanmrqs@users.noreply.github.com>
2026-04-30 16:28:43 -0300

    feat: ajustando datas

--- pages/6_History_Page.py
+++ pages/6_History_Page.py
@@ -2,6 +2,8 @@ import requests
 import api
 import streamlit as st
 from app import sidebar
+import pandas as pd
+import datetime as dt
 
 sidebar()
 st.title('History Page')
@@ -16,8 +18,12 @@ def users():
 user = st.session_state.get('user_name')
 try:
     h = api.get(f"/history/{user}")
+    df = pd.DataFrame(h)
+    df['datetime'] = pd.to_datetime(df['datetime'])
+    df['datetime'] = df['datetime'].dt.tz_convert('America/Sao_Paulo')
+    df['datetime'] = df['datetime'].dt.strftime('%d/%m/%Y %H:%M')
     st.subheader(f"{user}'s History")
-    st.dataframe(h)
+    st.dataframe(df)
 except requests.exceptions.HTTPError:
     st.error(f"{user}, you don't have a history, please log to see")
 
@@ -33,10 +39,14 @@ compare_name = other_name.split('-')
 if st.button('Search'):
         try:
             h = api.get(f"/history/{compare_name[1].strip()}")
+            df = pd.DataFrame(h)
+            df['datetime'] = pd.to_datetime(df['datetime'])
+            df['datetime'] = df['datetime'].dt.tz_convert('America/Sao_Paulo')
+            df['datetime'] = df['datetime'].dt.strftime('%d/%m/%Y %H:%M')
             if compare_name[1].strip() == user:
                 raise NameError
             st.subheader(f'{compare_name[1]} History')
-            st.dataframe(h)
+            st.dataframe(df)
         except requests.exceptions.HTTPError:
             st.error(f"{compare_name[1]} hasn't logged any workouts yet")
         except NameError:
